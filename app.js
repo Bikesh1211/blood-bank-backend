5b2390f985f351b0984b89acf567fabaa8061c2c
bikesh2023 <bchaudhary@eeposit.com>
2024-02-02 17:23:51 +0545

    fix: cors error

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const { getUsers } = require("./src/controller/users");
 
 const onRequest = async (req, res) => {
   const { method, url } = req;
-  if (method === "GET" && url === "/") {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  if (method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Content-Range, Content-Disposition, Content-Description"
+    );
+    res.end();
+  } else if (method === "GET" && url === "/") {
     res.setHeader("Content-Type", "text/html");
     const filePath = path.join(__dirname, "./public/template/index.html");
     try {
@@ -19,27 +27,38 @@ const onRequest = async (req, res) => {
       res.writeHead(500);
       res.end("Internal Server Error");
     }
-  }
-
-  if (method === "GET" && url === "/users") {
-    // res.setHeader("Content-Type", "application/json");
-    // const body = {
-    //   email: "bikesh@gmail.com1",
-    // };
-    // try {
-    //   const data = await userService.getUsers(body);
-    //   res.writeHead(200);
-    //   res.end(JSON.stringify(data));
-    // } catch (error) {
-    //   res.writeHead(500);
-    //   res.end("Internal Server Error");
-    // }
+  } else if (method === "GET" && url === "/users") {
     await getUsers(req, res);
+  } else if (req.method === "POST" && req.url === "/login") {
+    console.log("bikesh is inside post login");
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", async () => {
+      try {
+        const postData = JSON.parse(body);
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+
+        const user = await userService.loginUser(postData);
+        res.end(
+          JSON.stringify({ user, message: "Data received successfully" })
+        );
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+      }
+    });
   } else {
     res.writeHead(404);
     res.end(JSON.stringify({ error: "Not Found" }));
   }
 };
+
 const serverRunning = () => {
   console.log("server listening on port" + PORT);
 };
